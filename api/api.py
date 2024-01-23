22bb541c6bdf6b3c389c5413030178a7ae5858cb
jx0830 <jiaxin_0830@outlook.com>
2024-01-23 14:17:37 +0800

    use react-bootstrap

--- api/api.py
+++ api/api.py
@@ -4,7 +4,9 @@ from fastapi.middleware.cors import CORSMiddleware
 import torch
 from pydantic import BaseModel
 from torch import autocast
-from diffusers import StableDiffusionPipeline
+from diffusers import StableDiffusionPipeline, StableDiffusionControlNetPipeline, ControlNetModel, UniPCMultistepScheduler
+import numpy as np
+from diffusers.utils import load_image
 from io import BytesIO
 import base64 
 
@@ -17,21 +19,27 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-device = "cuda"
-model_id = "CompVis/stable-diffusion-v1-4"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, revision="fp16", torch_dtype=torch.float16, use_auth_token=auth_token)
-pipe.to(device)
+negative_prompt = '''nsfw, deformed, mutated, mutilated, distorted, disfigured extra limbs, missing limbs, floating limbs, disconnected body parts,
+                    missing arms, missing legs, missing fingers, amputated, amputation, extra arms, third arm, extra legs,too many fingers, fused fingers,
+                    low quality, low resolution, pixelated, jpeg artifacts, blurry, unclear, out of focus, depth of field, bad anatomy, wrong anatomy
+                    bad proportions, disproportionate  '''
 
 class FormValues(BaseModel):
     prompt: str
-    height: int | None = None
-    width: int | None = None
+    height: int
+    width: int
+    guidance_scale : float
+    steps : int
 
 @app.post("/")
-async def generate(formValue: FormValues): 
-    print(formValue)
+async def generate(formValues: FormValues): 
+    device = "cuda"
+    model_id = "CompVis/stable-diffusion-v1-4"
+    pipe = StableDiffusionPipeline.from_pretrained(model_id, revision="fp16", torch_dtype=torch.float16, use_auth_token=auth_token)
+    pipe.to(device)
+    print(formValues)
     with autocast(device): 
-        image = pipe(formValue.prompt, guidance_scale=8.5,height= 512,width= 768).images[0]
+        image = pipe(formValues.prompt, guidance_scale=formValues.guidance_scale, height=formValues.height, width=formValues.width, num_inference_steps=formValues.steps, negative_prompt=negative_prompt).images[0]
 
     image.save("testimage.png")
     buffer = BytesIO()
@@ -39,3 +47,9 @@ async def generate(formValue: FormValues):
     imgstr = base64.b64encode(buffer.getvalue())
 
     return Response(content=imgstr, media_type="image/png")
+
+@app.post("/controlNet")
+async def generate(formValues: FormValues): 
+    image = load_image(
+        "https://hf.co/datasets/huggingface/documentation-images/resolve/main/diffusers/input_image_vermeer.png")
+    
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,21 @@
-import {
-  ChakraProvider,
-  Heading,
-  Container,
-  Text,
-  Input,
-  Button,
-  Wrap,
-  Stack,
-  Image,
-  SkeletonCircle,
-  SkeletonText,
-  FormLabel 
-} from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Form, Button, Spinner } from "react-bootstrap";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
-import { useEffect, useState } from "react";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 const validationSchema = Yup.object({
-  prompt: Yup.string().required("Prompt is required"),
-  height: Yup.number().required("Height is required"),
-  width: Yup.number().required("Width is required"),
+  height: Yup.number()
+    .required("Height is required")
+    .integer("Height must be an integer")
+    .min(512, "Height cannot be lesser than 512"),
+  width: Yup.number()
+    .required("Width is required")
+    .integer("Height must be an integer")
+    .min(512, "Width cannot be lesser than 512"),
+  steps: Yup.number()
+    .required("Height is required")
+    .integer("Height must be an integer"),
 });
 
 const App = () => {
@@ -32,6 +27,8 @@ const App = () => {
       prompt: "",
       height: 512,
       width: 512,
+      guidance_scale: 7.5,
+      steps: 20,
     },
     validationSchema: validationSchema,
     onSubmit: (values) => {
@@ -41,6 +38,7 @@ const App = () => {
 
   const generate = async (formValues) => {
     updateLoading(true);
+    console.log("hello");
     try {
       const result = await axios.post("http://127.0.0.1:8000/", formValues);
       updateImage(result.data);
@@ -56,52 +54,127 @@ const App = () => {
   }, [formik.values]);
 
   return (
-    <ChakraProvider>
-      <Container>
-        <Heading>Stable Diffusion</Heading>
-        <br />
-      </Container>
-      <Container display="flex" justifyContent="space-between" maxW="1200px" padding={0}>
+    <Container>
+      <Row className="mb-3">
         {/* Left Section */}
-        <div style={{ flex: "1", padding: 0 }}>
-          <Heading>Left Section</Heading>
-          <form onSubmit={formik.handleSubmit}>
-            <Wrap marginBottom={"10px"}>
-              <FormLabel htmlFor="prompt">Prompt:</FormLabel>
-              <Input
-                type="text"
-                id="prompt"
-                name="prompt"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.prompt}
-                width={"350px"}
-              />
-              <Button type="submit" colorScheme={"yellow"}>
-                Generate
-              </Button>
-            </Wrap>
-  
-            {formik.touched.prompt && formik.errors.prompt ? (
-              <Text color="red">{formik.errors.prompt}</Text>
-            ) : null}
-          </form>
-        </div>
-  
+        <Col sm={6}>
+          <h1>Left Section</h1>
+
+          <Form onSubmit={formik.handleSubmit}>
+            <Row className="mb-3">
+              <Form.Group controlId="prompt">
+                <Form.Label>Prompt:</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.prompt}
+                  width={"350px"}
+                />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Col>
+                <Form.Group controlId="height">
+                  <Form.Label>Height:</Form.Label>
+                  <Form.Control
+                    type="number"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.height}
+                    width={"100px"}
+                    min={512}
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="width">
+                  <Form.Label>Width:</Form.Label>
+                  <Form.Control
+                    type="number"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.width}
+                    width={"100px"}
+                    min={512}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Col>
+                <Form.Group controlId="guidance_scale">
+                  <Form.Label>Guidance Scale:</Form.Label>
+                  <Form.Control
+                    type="number"
+                    step="0.1"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.guidance_scale}
+                    width={"100px"}
+                  />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="steps">
+                  <Form.Label>Steps:</Form.Label>
+                  <Form.Control
+                    type="number"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.steps}
+                    width={"100px"}
+                    min={10}
+                  />
+                </Form.Group>
+              </Col>
+            </Row>
+            <Row className="mb-3">
+              <Button type="submit">Generate</Button>
+            </Row>
+          </Form>
+        </Col>
+
         {/* Right Section */}
-        <div style={{ flex: "1" }}>
-          <Heading>Right Section</Heading>
-          {loading ? (
-            <Stack>
-              <SkeletonCircle />
-              <SkeletonText />
-            </Stack>
-          ) : image ? (
-            <Image src={`data:image/png;base64,${image}`} boxShadow="lg" />
-          ) : null}
-        </div>
-      </Container>
-    </ChakraProvider>
+        <Col sm={6}>
+          <h1>Right Section</h1>
+          <div
+            style={{
+              boxShadow: "lg",
+              border: "1px solid #ccc",
+              borderRadius: "md",
+              height: "400px",
+              width: "100%",
+              overflow: "hidden", // Hide any overflow
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
+              }}
+            >
+              {loading ? (
+                <Spinner animation="border" variant="primary" />
+              ) : image ? (
+                <img
+                  src={`data:image/png;base64,${image}`}
+                  alt="Generated"
+                  style={{
+                    maxWidth: "100%",
+                    maxHeight: "100%",
+                    width: "auto",
+                    height: "auto",
+                  }}
+                />
+              ) : null}
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
